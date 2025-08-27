f74cec02b45d38053feba3533268f9e1edadcf05
Asa <asaelrddz00@gmail.com>
2025-08-26 22:58:33 -0600

    state hoisting and lambda

--- app/src/main/java/com/learningjetpack/state/MyState.kt
+++ app/src/main/java/com/learningjetpack/state/MyState.kt
@@ -12,11 +12,27 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun MyState(modifier: Modifier) {
+
+    /**
+     * rememberSaveable: mantiene datos (giro de pantalla)
+     *
+     * by: ya no tener que hacer number.value
+     **/
     var number by rememberSaveable { mutableStateOf(0) }
     //var number  =  remember { mutableStateOf(0) }
 
-    Column {
-        Text("Pulsame: $number", modifier.clickable { number += 1 })
-        Text("Pulsame: $number", modifier.clickable { number += 1 })
+    Column(modifier = modifier) {
+        StateExample1(number) { number += 1 }
+        StateExample2(number, onClick = { number += 1 })
     }
+}
+
+@Composable
+fun StateExample1(number : Int, onClick: () -> Unit) {
+    Text("Pulsame: $number", Modifier.clickable { onClick() })
+}
+
+@Composable
+fun StateExample2(number : Int, onClick: () -> Unit) {
+    Text("Pulsame: $number", Modifier.clickable { onClick() })
 }
