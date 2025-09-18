cdbeb85c7fb7a258a32e9726c756eeaab302ba8b
Asa <asaelrddz00@gmail.com>
2025-09-17 22:01:03 -0600

    BackHandler

--- app/src/main/java/com/learningjetpack/learning/components/navigation/examples/LoginScreen.kt
+++ app/src/main/java/com/learningjetpack/learning/components/navigation/examples/LoginScreen.kt
@@ -1,27 +1,46 @@
 package com.learningjetpack.learning.components.navigation.examples
 
 import android.widget.Space
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen(navigateToDetail: () -> Unit) {
+    var show by remember { mutableStateOf(false) }
+
     Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Spacer(Modifier.weight(1f))
         Text("LOGIN", fontSize = 30.sp)
         Spacer(Modifier.weight(1f))
         Button(onClick = {
-            navigateToDetail()
+            show = true
+            //navigateToDetail()
         }) {
             Text("Navegar")
         }
         Spacer(Modifier.weight(1f))
+    }
+
+    if (show) {
+        // control de boton para atras
+        BackHandler {
+            show = false
+        }
+        Box(Modifier.fillMaxSize().background(Color.Red))
     }
 }
